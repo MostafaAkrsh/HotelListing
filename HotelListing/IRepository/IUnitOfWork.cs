6ef499af971b4a3688436b402862222d03b4c7ed
Mostafa Samir <mostafasamir355@gmail.com>
2021-05-10 15:30:57 +0200

    Added Database, AutoMapper, Unit OF Work and DTOs

---
+++ HotelListing/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using HotelListing.Data;
+using HotelListing.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repositroy
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+
+        Task Save();
+    }
+}
